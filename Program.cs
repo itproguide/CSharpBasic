3e2a188d6599bd2fa31124c4cd1a5f0376855864
Mohammed Niyas <43110927+itproguide@users.noreply.github.com>
2022-05-26 10:19:28 +0400

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,45 @@
+namespace SimpleProgram1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Display Student Details");
+            //Console.WriteLine("Name: ");
+            //Console.WriteLine("Mohammed Niyas");
+            //Console.WriteLine("Branch:  ");
+            //Console.WriteLine("ECE");
+            //Console.WriteLine("Score:  ");
+            //Console.WriteLine("80%");
+            //Console.WriteLine("Year: ");
+            //Console.WriteLine("2010");
+            //Console.WriteLine("Status: ");
+            //Console.WriteLine("Pass");
+
+            //string name = "Mohammed Niyas";
+            //int score = 80;
+            //int year = 2010;
+            //Console.WriteLine("Display Student Details");
+            //Console.WriteLine("Name: ");
+            //Console.WriteLine(name);
+            //Console.WriteLine("Branch:  ");
+            //Console.WriteLine("ECE");
+            //Console.WriteLine("Score:  ");
+            //Console.WriteLine(score);
+            //Console.WriteLine("Year: ");
+            //Console.WriteLine(year);
+            //Console.WriteLine("Status: ");
+            //Console.WriteLine("Pass");
+
+            string name;
+            Console.WriteLine("Display Student Details");
+            Console.WriteLine("Name: ");
+            name = Console.ReadLine();
+            Console.WriteLine(name);
+            Console.WriteLine("Branch:  ");
+            Console.WriteLine("ECE");
+            Console.WriteLine("Status: ");
+            Console.WriteLine("Pass");
+        }
+    }
+}
